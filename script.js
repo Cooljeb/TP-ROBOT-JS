3aedc1e64a1e9b35c67ed819af8609cf2b3c2c37
Votre Nom <h.hassen.malek@gmail.com>
2024-12-13 14:29:49 +0100

    creation methode afficher grille

--- script.js
+++ script.js
@@ -64,15 +64,33 @@ class Robot {
   }
 }
 
+
+
+
 class Grille {
   constructor(width, height) {
     this.width = width;
     this.height = height;
   }
-
-  afficherGrille() {
-    console.log(`Grille de ${this.width} x ${this.height}`);
-  }
+  initGrille(width, height, pourcentage) {
+    let colonne = [];
+    for (let i = 0; i < height; i++) {
+        let ligne = [];
+        for (let j = 0; j < width; j++) {
+            if( i ==0 && j == 0){
+                ligne.push("R");
+            }
+        if (Math.random() < pourcentage) {
+            ligne.push("X");
+        } else {
+            ligne.push("  ");
+        }
+        //console.log(ligne[1], colonne[2]);
+        }
+        colonne.push(ligne.join("")); // Combine chaque ligne en une chaîne
+    }
+    return colonne.join("\n"); // Combine les lignes pour former un carré
+    }
 }
 
 let width = 100;
@@ -80,26 +98,12 @@ let height = 200;
 const robot = new Robot("R2D2"); // Création d'un robot
 const grille = new Grille(width, height); // Création d'une grille
 
-grille.afficherGrille();
 
-function dessinerGrille(width, height, pourcentage) {
-  let colonne = [];
+let g = new Grille(5, 5);
 
-  for (let i = 0; i < height; i++) {
-    let ligne = [];
-    for (let j = 0; j < width; j++) {
-      //if (j % 2 === 0) {
-      if (Math.random() < pourcentage) {
-        ligne.push("X");
-      } else {
-        ligne.push("  ");
-      }
-      //console.log(ligne[1], colonne[2]);
-    }
-    colonne.push(ligne.join("")); // Combine chaque ligne en une chaîne
-  }
+console.log(g.afficherGrille(5,5,0.5));
 
-  return colonne.join("\n"); // Combine les lignes pour former un carré
-}
 
-console.log(dessinerGrille(5, 5, 0.9));
+
+
+
